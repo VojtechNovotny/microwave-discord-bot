c3cf0f8e3ff199c68918ef31bd00ca3baaf397b3
VojtechNovotny <vojta.nvtn@gmail.com>
2024-05-29 11:40:18 +0200

    Bot setup

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("brrrrrrrrrrrr");
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { config } from 'dotenv';
+
+config();
+
+// Create a new client instance
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds],
+});
+
+// When the client is ready, run this code (only once)
+client.once(Events.ClientReady, readyDiscord);
+
+// Login to Discord with your client's token
+client.login(process.env.TOKEN);
+
+function readyDiscord() {
+    console.log("brrrrrrrrr");
+}
